28a16c22db8ed9bf91fb5727e38a93297eb8a0ef
Sean O'Brien <seanob01@nssrstudio03.csa.scot.nhs.uk>
2021-10-27 16:39:32 +0100

    Reactable and year on year changes working well. Also a info bar put in

---
+++ table_output.R
@@ -0,0 +1,93 @@
+#========================================================
+# Takes a table with first column as a variable
+# and all subsequent columns are observations and
+# returns a reactable table with the data and a 
+# sparkline for each row
+#--------------------------------------------------------
+table_with_sparklines <- function(table_data, p_m_colouring = FALSE){
+  table_data_t <- table_data %>%
+    pivot_longer(-area, names_to = "year_ending") %>% 
+    pivot_wider(names_from = area, values_from = value)
+  
+  year_end_group <- names(table_data) %>% tail(-1)
+  
+  #c(pos_max, neg_max)
+  abs_maxs <- get_abs_p_m_max(table_data)
+  
+  table_data <- table_data %>% 
+    mutate(sparkline=NA)
+  
+  #inital columns that we don't need to iterate over
+  col_defs <- list(
+    area = colDef(sticky="left", name = "Area", width = 200),
+    
+    sparkline = colDef(name = "Trend Lines",
+                       sticky = "right",
+                       cell = function(value, index) {
+                         sparkline(table_data_t[[table_data[[index,1]]]])
+                       })
+                        )
+  
+  if (p_m_colouring) {
+    #all the year end columns that are identical
+    for (year_end_col in year_end_group){
+      col_defs[[year_end_col]] <- make_year_end_column(abs_max_vals = abs_maxs)
+    }
+  }
+  
+  
+  reactable(table_data, columns = col_defs,
+  columnGroups = list(
+    colGroup(name="Year Ending", columns=year_end_group)
+    )
+  )
+}
+
+
+
+make_p_m_color_pal <- function(plus_neg_zero_colours, bias = 1) {
+  p_col <- plus_neg_zero_colours[[1]]
+  n_col <- plus_neg_zero_colours[[2]]
+  z_col <- plus_neg_zero_colours[[3]]
+  
+  get_colour_p <- colorRamp(c(z_col, p_col), bias = bias)
+  get_colour_n <- colorRamp(c(z_col, n_col), bias = bias)
+  
+  function(x) {
+    if (x==0){
+      #usually white
+      z_col
+    } else if (x < 0){
+      rgb(get_colour_n(abs(x)), maxColorValue = 255)
+    } else {
+      rgb(get_colour_p(x), maxColorValue = 255)
+    }
+  }
+}
+
+p_m_colours <- make_p_m_color_pal(c("#d26146", "#9cc951", "#ffffff"), bias=1.5)
+
+make_year_end_column <- function(abs_max_vals){
+  colDef(style = function(value){
+    pos_max <- abs_max_vals[[1]]
+    neg_max <- abs_max_vals[[2]]
+    
+    if (value==0){
+      scaled <- 0
+    } else if (value > 0){
+      scaled <- value/pos_max
+    } else {
+      scaled <- value/neg_max
+    }
+    
+    list(color = "#111", background = p_m_colours(scaled))
+  },
+  format = colFormat(percent = TRUE))
+}
+
+#bit of a bodge. Doesn't handle stupid cases very well
+get_abs_p_m_max <- function(table){
+  neg_max <- table %>% select_if(is.numeric) %>% min() %>% abs()
+  pos_max <- table %>% select_if(is.numeric) %>% max() %>% abs()
+  c(pos_max, neg_max)
+}
--- trend_table.R
+++ trend_table.R
@@ -6,14 +6,8 @@
 # the column val_name
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 trend_table_data <- function(data, val_name){
-  big_areas <- c("Scotland", "NHS Greater Glasgow and Clyde")
   data %>%
-    #convert date time to string based on template
-    mutate(date_end = format(date_end, format="%d-%b-%y")) %>%
-    select(c("area", "date_end", val_name)) %>%
-    pivot_wider(names_from = "date_end", values_from = val_name) %>%
-    #put big areas at bottom
-    arrange(area %in% big_areas, area)
+    format_and_pivot(val_name)
 }
 
 
@@ -21,157 +15,50 @@ trend_table_data <- function(data, val_name){
 # Makes a relative change (percentage of first column) table from data
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 relative_change_table_data <- function(data, val_name){
+  data %>%
+    format_and_pivot(val_name) %>% 
+    # everything as percentage of first date (column 2)
+    mutate_if(is.numeric, change_percentage, .[[2]]) %>%
+    mutate_if(is.numeric, round, 2)
+}
+
+
+year_on_year_change <- function(data, val_name){
+  data_i <- data %>% 
+    format_and_pivot(val_name)
+  
+  data_f <- data_i
+  for (i in seq_along(data_i)){
+    #first column is places,
+    #second is initial year (no previous)
+    if (i>2) {
+      prev <- data_i[i-1]
+      cur <- data_i[i]
+      data_f[i] <- (cur-prev)/prev
+    }
+  }
+  data_f <- data_f %>% mutate_if(is.numeric, round, 3)
+  data_f[2] <- 0  
+  data_f
+}
+
+
+
+format_and_pivot <- function(data, val_name){
   big_areas <- c("Scotland", "NHS Greater Glasgow and Clyde")
   data %>%
     #convert date time to string based on template
     mutate(date_end = format(date_end, format="%d-%b-%y")) %>%
     select(c("area", "date_end", val_name)) %>%
     pivot_wider(names_from = "date_end", values_from = val_name) %>%
-    # everything as percentage of first date (column 2)
-    mutate_if(is.numeric, change_percentage, .[[2]]) %>%
-    mutate_if(is.numeric, round, 2) %>% 
     #put big areas at bottom
     arrange(area %in% big_areas, area)
 }
 
+
+
 change_percentage <- function(col, init_col){
-  round((col/init_col - 1)*100,2)
+  round((col/init_col - 1),2)
 }
-
-
-
-
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-# Runs generate_trend_plots over each row in
-# trend_table_data.
-# This MUST be run in a chunk with the option include=FALSE
-# This will allow for the plots to be generated but not shown
-# as they only need to be shown in the table
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-make_all_plots <- function(data, val_name, table_generator){
-  par(oma=c(0,0,0,0))
-  par(mar=c(0,0,0,0))
-  
-  table_data <- data %>% table_generator(val_name)
-  
-  #generate the plots
-  apply(table_data[,2:ncol(table_data)], 1, generate_trend_plots)
-}
-
-
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-# Generates and displays the final table with the plots
-# embedded in the last column
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-make_trend_table <- function(trend_table_data){
-  #add the plots (filepaths pointing to them) to the data
-  out <- cbind(trend_table_data, sprintf("![](%s%s-%s.png){width=45%%}", opts_current$get("fig.path"), opts_current$get("label"), 1:nrow(trend_table_data)))
-  
-  #you NEED the column name to not be "![](%s%s-%s.png){width=45%%}" which is what it is automatically assigned as
-  names(out) <- append(names(out)[1:length(names(out))-1], "Trend lines")
-  
-  out %>%
-    kable() %>%
-    kable_styling() %>% 
-    column_spec(1, width = "15em") %>%
-    row_spec(c(7,8), hline_after = TRUE, color = phs_colours("phs-teal-80"), background = phs_colours("phs-teal-10")) %>%
-    column_spec(ncol(out), width="20em", border_left = TRUE, background="white") %>% 
-    add_header_above(c(" " = 1, "Year Ending" = 9, "  "=1))
-}
-
-
-
-
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-make_relative_change_table <- function(relative_change_table_data){
-  
-  abs_max <- relative_change_table_data %>% 
-    select_if(is.numeric) %>% 
-    abs() %>% 
-    max()
-  
-  out <- relative_change_table_data %>% 
-    mutate_if(is.numeric, change_colourings, abs_max)
-  
-  #add the plots (filepaths pointing to them) to the data
-  out <- cbind(out, sprintf("![](%s%s-%s.png){width=45%%}", opts_current$get("fig.path"), opts_current$get("label"), 1:nrow(out)))
-  
-  #you NEED the column name to not be "![](%s%s-%s.png){width=45%%}" which is what it is automatically assigned as
-  names(out) <- append(names(out)[1:length(names(out))-1], "Trend lines")
-  
-  out %>%
-    kable(escape=FALSE) %>%
-    kable_styling() %>% 
-    column_spec(1, width = "15em") %>%
-    row_spec(c(7,8), hline_after = TRUE, color = phs_colours("phs-teal-80")) %>%
-    column_spec(ncol(out), width="20em", border_left = TRUE) %>% 
-    add_header_above(c(" " = 1, "Year Ending" = 9, "  "=1))
-}
-
-change_colourings <- function(x, abs_max){
-  alphas <- abs(x/abs_max)
-  cell_spec(x, background = case_when(!is.numeric(x)~"white",
-                                      x>0 ~ rgb(199/255,57/255,24/255, alphas),
-                                      x<0 ~ rgb(0/255,120/255,212/255, alphas),
-                                      TRUE ~ "white"))
-}
-
-
-
-
-
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-# Makes a single trend plot from a row vector
-#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-generate_trend_plots <- function(col){
-  print("yo")
-  col_len = length(col)
-  data = tibble(x=1:col_len, y=col)
-  
-  max_y = data %>% pull(y) %>% max()
-  #first instance of row where y is maximum
-  max_x = data %>% filter(y==max_y) %>% pull(x) %>% min()
-  
-  min_y = data %>% pull(y) %>% min()
-  #first instance of row where y is minimum
-  min_x = data %>% filter(y==min_y) %>% pull(x) %>% min()
-  
-  colours <- list(rep("black", col_len))
-  colours[min_x] <- "blue"
-  colours[max_x] <- "red"
-  
-  min_max <- data %>% slice(c(min_x,max_x)) %>% mutate(type=c("min", "max"))  
-  
-  data %>% ggplot(aes(x=x,y=y)) +
-    geom_line(size=0.4, colour="#CAC6D1") +
-    geom_point(data=min_max, aes(x=x,y=y, colour=type, shape="")) +
-    
-    theme(axis.title.x=element_blank(),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          
-          axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank(),
-          
-          axis.line =element_blank(),
-          
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          
-          plot.margin = unit(c(0, 0, 0, 0), "null"),
-          panel.margin = unit(c(0, 0, 0, 0), "null"),
-          
-          legend.position = "none",
-          legend.margin = unit(0, "null"))
-  
-}
-
-
-
 
 
