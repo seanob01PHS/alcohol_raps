b2caebfd23a1a0705e50f429948374ad12e95856
Sean O'Brien <seanob01@nssrstudio04.csa.scot.nhs.uk>
2021-11-19 10:10:27 +0000

    Somewhat working details sub-table. Some strange bugs related to function environments

--- profile_report/process_wrangle.R
+++ profile_report/process_wrangle.R
@@ -1,58 +1,108 @@
-year_type <- function(year_str){
-  case_when(grepl("-", year_str) ~ "3 cal year agg",
-            grepl(" to ", year_str) ~ "3 fin year agg",
-            grepl("/", year_str) ~ "fin year",
-            TRUE ~ NA_character_)
-}
-
-
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Just a little wrapper really
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 process_year <- function(data){
   data %>% 
-    mutate(year_type=year_type(year),
-           year_start=as.numeric(substr(year, 1, 4))) %>% 
+    mutate(year_start=as.numeric(substr(year, 1, 4))) %>% 
     rename(year_str=year)
 
 }
 
 
 
-
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Gives the most up to date `year_start` for a given
+# indicator 
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 most_up_to_date_date <- function(data, indicator_f){
   data %>% filter(indicator==indicator_f) %>% pull(year_start) %>% max()
 }
 
 
 
-
-
-rate_pairings <- function(data, most_recent_dates){
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# returns a list of [indicator, indicator_cur_year, indicator_prev_year]
+# to help in calculating "difference from previous year" columns
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+rate_pairings <- function(data){
   #list of [[recent_col_1, old_col_1], [recent_col_2, old_col_2]...]
   column_pairings <- list()
-  for (indicator_f in data %>% pull(indicator) %>% unique()){
-    cur_year_str <- data %>% 
-      filter(indicator==indicator_f,
-             year_start==most_recent_dates[indicator]) %>% 
-      pull(year_str) %>% 
-      .[[1]]
+  
+  #names of columns that represent a value (not a difference etc)
+  names_of_vals <- data %>% names()
+  #starts with "val||"
+  names_of_vals <- Filter(function(x) grepl("^(val\\|\\|)", x), names_of_vals)
+  
+  col_infos <- val_col_infos(names_of_vals)
+  indicators <- col_infos[[1]]
+  year_strs <- col_infos[[2]]
+  year_starts <- col_infos[[3]]
+  
+  for (indicator in indicators %>% unique()){
+    indicator_indexs <- which(indicator == indicators)
     
-    prev_year_str <- data %>% 
-      filter(indicator==indicator_f,
-             year_start==unlist(most_recent_dates[indicator])-1) %>% 
-      pull(year_str) %>% 
-      .[[1]]
+    max_year_start <- year_starts[indicator_indexs] %>% max()
+    min_year_start <- year_starts[indicator_indexs] %>% min()
     
-    column_pairings[[length(column_pairings)+1]] <- list(indicator_f,
-                                                         paste(indicator_f, cur_year_str),
-                                                         paste(indicator_f, prev_year_str))
+    max_year_str <- year_strs[[which(indicator==indicators & max_year_start==year_starts)]]
+    min_year_str <- year_strs[[which(indicator==indicators & min_year_start==year_starts)]]
+
+    
+    column_pairings[[length(column_pairings)+1]] <- list(indicator,
+                                                         paste0("val||", indicator, "||", max_year_str, "||", max_year_start),
+                                                         paste0("val||", indicator, "||", min_year_str, "||", min_year_start))
   }
+
   column_pairings
 }
 
 
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Returns the first non-NA element in a list
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+val_col_infos <- function(value_col_names){
+  col_info <- str_match_all(value_col_names, "[^\\|]+")
+  indicators <- col_info %>% map_chr(2)
+  year_strs <- col_info %>% map_chr(3)
+  year_starts <- col_info %>% map_chr(4) %>% as.numeric()
+  
+  list(indicators, year_strs, year_starts)
+}
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Returns the first non-NA element in a list
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+first_non_na <- function(grp){
+  first(na.omit(grp))
+}
+
+
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Vectorised rate of change where
+#     a is (a/b-1) change from b
+#     0 is (0) change from 0
+#     a is (NA) change from 0
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+zero_safe_rate_of_change <- function(a,b){
+  case_when(a>0 & b==0 ~ NA_real_,
+            a==0 & b==0 ~ 0.,
+            TRUE ~ a/b - 1)
+}
+
+
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Returns a df with all the columns ready to be displayed as they are
+# in the profile tab of the alcohol harms workbook
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 make_profile_frame <- function(data){
   
+  # A char vector of all the different types of  indicators in the data
   indicators <- data %>% pull(indicator) %>% unique()
   
+  # A list of l[[indicator]] <- the most up to date `year_start` for that 
+  #                               given indicator
   most_recent_dates <- list()
   for (indicator in indicators){
     most_recent_dates[[indicator]] <- most_up_to_date_date(data, indicator)
@@ -63,20 +113,22 @@ make_profile_frame <- function(data){
     filter(year_start == most_recent_dates[indicator] |
            year_start == unlist(most_recent_dates[indicator]) - 1)
   
+  # Column names are coded as
+  # "val"||indicator||Year string||year start corr. to that year string
   data_piv <- data_f %>% 
-    mutate(indicator_and_year_str = paste(indicator, year_str)) %>% 
-    select(iz, iz_name, hscp, indicator_and_year_str, rate) %>% 
-    pivot_wider(names_from = indicator_and_year_str, values_from = rate)
+    mutate(name_code_str = paste0("val||", indicator, "||", year_str, "||", year_start)) %>% 
+    select(iz, iz_name, hscp, name_code_str, rate) %>% 
+    pivot_wider(names_from = name_code_str, values_from = rate)
   
   
-  rate_pairings <- rate_pairings(data_f, most_recent_dates)
+  rate_pairings <- rate_pairings(data_piv)
   
   for (pair in rate_pairings){
     indicator_name <- pair[[1]]
     curr_col <- pair[[2]]
     old_col <- pair[[3]]
     
-    new_var_name <- paste(indicator_name, "rate of change")
+    new_var_name <- paste0("roc||", indicator_name)
     
     data_piv[[new_var_name]] <- zero_safe_rate_of_change(data_piv[[curr_col]], data_piv[[old_col]])
   }
@@ -109,33 +161,17 @@ make_profile_frame <- function(data){
     indicator <- pair[[1]]
     curr_col <- pair[[2]]
     
-    new_var_name_scot <- paste(indicator, " difference compared to Scotland")
-    new_var_name_ggc <- paste(indicator, " difference compared to NHS GGC")
+    new_var_name_scot <- paste("diff vs||Scotland||", indicator)
+    new_var_name_ggc <- paste("diff vs||NHS GGC||", indicator)
     
     data_piv[[new_var_name_scot]] <- zero_safe_rate_of_change(data_piv[[curr_col]], scot_figs[[indicator]])
     data_piv[[new_var_name_ggc]] <- zero_safe_rate_of_change(data_piv[[curr_col]], ggc_figs[[indicator]])
     
   }
-  
-  
-  data_piv_f <- data_piv %>%
-    pivot_longer(cols = contains("compared to"), names_to = "full_comparison", values_to = "comparison_ratio") %>% 
-    mutate(compared_to = full_comparison %>% str_split(" compared to ") %>% map_chr(2),
-           comparison_indicator = full_comparison %>% str_split(" compared to ") %>% map_chr(1)) %>% 
-    select(-full_comparison) %>% 
-    pivot_wider(names_from = "comparison_indicator", values_from = "comparison_ratio")
 
   
-  data_piv_f %>% select(-all_of(unlist(old_cols)))
-}
-
-first_non_na <- function(grp){
-  first(na.omit(grp))
+  data_piv %>% select(-all_of(unlist(old_cols)))
 }
 
 
-zero_safe_rate_of_change <- function(a,b){
-  case_when(a>0 & b==0 ~ NA_real_,
-            a==0 & b==0 ~ 0.,
-            TRUE ~ a/b - 1)
-}
+
--- profile_report/table_output_profile.R
+++ profile_report/table_output_profile.R
@@ -1,95 +1,125 @@
 library(htmltools)
 
-profile_table <- function(table_data, shared_table_data){
+profile_table <- function(table_data, all_data){
   big_areas <- c("Scotland", "NHS Greater Glasgow and Clyde", "NHS GGC")
   
-  #inital columns that we don't need to iterate over
-  col_defs <- list(
-    iz = colDef(sticky="left", name = "Intermediate Zone Code", width = 100, 
-                  style = function(value) {
-                    if (value %in% big_areas) {
-                      font <- "bold"
-                      background <- "#e6f2fb"
-                    } else {
-                      font <- "regular"
-                      background <- "#ffffff"
-                    }
-                    list(background = background, fontWeight = font, fontSize="8pt")
-                  },
-                headerStyle = list(fontSize="9pt")),
+  
+  col_names <- table_data %>% names
+  indicators <- Filter(function(x) grepl("^(val\\|\\|)", x), col_names) %>% 
+    str_match_all("[^\\|]+") %>% 
+    map_chr(2)
+  
+  
+  my_col_defs <- default_cols()
+  my_col_groups <- list()
+  
+  # Main loop where all the value and roc columns are generated
+  for (indicator in indicators){
+    val_col <- col_names[[which(grepl(paste0("^(val\\|\\|",
+                                             indicator, ")"),
+                                      col_names))]]
+    roc_col <- col_names[[which(grepl(paste0("^(roc\\|\\|",
+                                             indicator, ")"),
+                                      col_names))]]
     
-    compared_to=colDef(show = FALSE),
+    my_col_defs[[val_col]] <- make_val_column(table_data[val_col])
+    my_col_defs[[roc_col]] <- make_roc_column(table_data[roc_col])
     
-    iz_name = colDef(sticky="left", name = "Intermediate Zone Name", width=220,
-                     style=list(fontSize="8pt"),
-                     headerStyle = list(fontSize="9pt")),
     
-    hscp = colDef(sticky="left", name = "HSCP", width=150,
-                  style=list(fontSize="8pt"),
-                  headerStyle = list(fontSize="9pt"))
-  )
-  
-  #difference columns c(abs_pos_max, abs_neg_min)
-  comp_abs_maxs <- get_abs_p_m_max(table_data %>% select(contains("difference")))
-  
-  numbers_group <- table_data %>% 
-    select(matches("(Alcohol-).*\\d")) %>% 
-    colnames()
-  
-  for (col in numbers_group){
-    col_defs[[col]] <- colDef(format=colFormat(digits=2), style=list(fontSize="8pt"), headerStyle = list(fontSize="9pt"))
+    my_col_groups[[length(my_col_groups)+1]] <- 
+      colGroup(name = indicator, columns = c(val_col, roc_col))
   }
+
   
-  
-  roc_group <- table_data %>% 
-    select(matches("(rate of change)")) %>% 
-    colnames()
-  
-  difference_group <- table_data %>% 
-    select(matches("(difference")) %>% 
-    colnames()
-  
-  for (col in roc_group){
-    col_defs[[col]] <- make_roc_column(comp_abs_maxs)
-  }
-  
-  for (col in difference_group){
-    col_defs[[col]] <- make_difference_column(comp_abs_maxs)
-  }
-  
-  
-  
-  difference_col_group <- colGroup(columns = unlist(difference_group),
-                                   name= "Difference rel. to NHS GGC or Scotland")
-  
-  roc_col_group <- colGroup(columns = unlist(roc_group),
-                            name = "Rate of change rel. to previous year")
-                                   
-  reactable(shared_table_data,
-            columns = col_defs,
+   
+  reactable(table_data,
+            columns = my_col_defs,
+            defaultColDef = colDef(show = FALSE),
+            searchable = TRUE,
             compact = TRUE,
             pagination = FALSE,
-            columnGroups = list(
-               difference_col_group,
-               roc_col_group
+            columnGroups = my_col_groups,
+            details =   colDef(
+              show=TRUE,
+              details = details_generator(all_data)
             )
   )
 }
 
 
 
-#bit of a bodge. Doesn't handle stupid cases very well
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Generator for value columns
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+make_val_column <- function(col_data){
+  abs_max_vals <- get_abs_p_m_max(col_data)
+  colDef(
+    show = TRUE,
+    name = "Value",
+    headerStyle = list(fontSize="9pt"),
+    format=colFormat(digits=2),
+    align = "right",
+    class = "border-left",
+    style = comparison_style_generator(abs_max_vals),
+    html = TRUE,
+    header = function(header_val, col_name){
+      div(class = "val_and_roc_head",
+          header_val,
+          div(class = "val_and_roc_subhead",
+              "EASR")
+          )}
+  )
+}
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Generator for Rate Of Change columns
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+make_roc_column <- function(col_data){
+  abs_max_vals <- get_abs_p_m_max(col_data)
+  colDef(
+    show = TRUE,
+    name = "Rate of Change",
+    headerStyle = list(fontSize="9pt"),
+    format=colFormat(digits=2),
+    align = "right",
+    cell = format_pct,
+    style = comparison_style_generator(abs_max_vals),
+    html = TRUE,
+    header = function(header_val, col_name){
+      div(class = "val_and_roc_head",
+          header_val,
+          div(class = "val_and_roc_subhead",
+              "rel. prev. period")
+      )}
+  )
+}
+
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# To get the absolute value of the extremes of a data set
+# for colour scaling purposes
+# Returns c(abs(max(x)), abs(min(x)))
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 get_abs_p_m_max <- function(table){
-  neg_max <- table %>% select_if(is.numeric) %>% min() %>% abs()
-  pos_max <- table %>% select_if(is.numeric) %>% max() %>% abs()
+  neg_max <- table %>% select_if(is.numeric) %>% min(na.rm = TRUE) %>% abs()
+  pos_max <- table %>% select_if(is.numeric) %>% max(na.rm = TRUE) %>% abs()
   c(pos_max, neg_max)
 }
 
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Nice formatting of pct columns
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 format_pct <- function(value) {
   if (is.na(value)) "  \u2013  "    # en dash for NAs
   else formatC(paste0(round(value * 100), "%"), width = 6)
 }
 
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Makes the function that returns the style of a comparison
+# cell based on abs_max_vals
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 comparison_style_generator <- function(abs_max_vals){
   function(value){
     pos_max <- abs_max_vals[[1]]
@@ -108,38 +138,44 @@ comparison_style_generator <- function(abs_max_vals){
 }
 
 
-make_roc_column <- function(abs_max_vals){
-  colDef(
-    headerStyle = list(fontSize="9pt"),
-    format=colFormat(digits=2),
-    align = "right",
-    cell = format_pct,
-    style = comparison_style_generator(abs_max_vals),
-    html = TRUE,
-    header =JS("
-              function(colInfo) {
-              return colInfo.column.name + '<div style=\"color: #999\">cm</div>'
-              }")
-    )
-}
-
-
-
-make_difference_column <- function(abs_max_vals){
-  colDef(
-    headerStyle = list(fontSize="9pt"),
-    format=colFormat(digits=2),
-    align = "right",
-    cell = format_pct,
-    style = comparison_style_generator(abs_max_vals),
-    html = TRUE,
-    header =JS("
-              function(colInfo) {
-              return colInfo.column.name + '<div style=\"color: #999\">cm</div>'
-              }")
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Sets up the defaults (basic info) columns
+# Just to clear up space
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+default_cols <- function(){
+  list(
+    iz = colDef(sticky="left", name = "Intermediate Zone Code", width = 85, 
+                show = TRUE,
+                filterable = TRUE,
+                style = function(value) {
+                  if (value %in% big_areas) {
+                    font <- "bold"
+                    background <- "#e6f2fb"
+                  } else {
+                    font <- "regular"
+                    background <- "#ffffff"
+                  }
+                  list(background = background, fontWeight = font, fontSize="8pt")
+                },
+                headerStyle = list(fontSize="9pt")),
+    iz_name = colDef(sticky="left", name = "Intermediate Zone Name", width=220,
+                     filterable = TRUE,
+                     show = TRUE,
+                     style=list(fontSize="8pt"),
+                     headerStyle = list(fontSize="9pt")),
+    
+    hscp = colDef(sticky="left", name = "HSCP", width=125,
+                  filterable = TRUE,
+                  show = TRUE,
+                  style=list(fontSize="8pt"),
+                  headerStyle = list(fontSize="9pt"))
   )
 }
 
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# returns a function that returns different colours to positive
+# and negative values given a pre scaled value input
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 make_p_m_color_pal <- function(plus_neg_zero_colours, bias = 1) {
   p_col <- plus_neg_zero_colours[[1]]
   n_col <- plus_neg_zero_colours[[2]]
@@ -160,4 +196,98 @@ make_p_m_color_pal <- function(plus_neg_zero_colours, bias = 1) {
   }
 }
 
+
 p_m_colours <- make_p_m_color_pal(c("#d26146", "#9cc951", "#ffffff"), bias=1.5)
+
+
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Returns a list of details for each row
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+details_generator <- function(data){
+  details_data <- data %>% select(contains("diff vs||"))
+  details_col_defs <- list()
+  abs_max_col_vals <- list()
+  
+  
+  for (col_x in details_data %>% names %>% sort){
+    
+    if (grepl("(Scotland)", col_x)){
+      rel_to <- "Scotland"
+      #print("yoyo")
+    } else if (grepl("(NHS GGC)", col_x)){
+      rel_to <- "NHS GGC"
+    } else {
+      rel_to <- "ERROR"
+    }
+    
+    header_f <-  function(header_val, col_name){
+      div(class = "details_head",
+          header_val,
+          div(class = "details_subhead",
+              rel_to)
+      )}
+    
+    
+    details_col_defs[[col_x]] <- colDef(show = TRUE,
+                                        name = paste(indicator_from_diff_col(col_x), "difference"),
+                                        format = colFormat(digits=2),
+                                        html = TRUE,
+                                        cell = details_format_generator(get_abs_p_m_max(details_data[col_x])),
+                                        header = header_f
+                                        )
+    
+  }
+  
+  
+  function(index){
+    htmltools::div(style = "padding: 16px",
+                   reactable(details_data[index, ],
+                             columns = details_col_defs
+                             )
+    )
+  }
+}
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# For doing the renaming above
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+indicator_from_diff_col <- function(col_name){
+  indicator <- col_name %>% 
+    str_match_all("[^\\|]+") %>% 
+    .[[1]] %>% .[[3]]
+}
+
+
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+# Nice formatting for details
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+details_format_generator <- function(abs_max_vals){
+  function(value, row_index, col_name){
+    pos_max <- abs_max_vals[[1]]
+    neg_max <- abs_max_vals[[2]]
+    
+    
+    pos_max <- 10
+    neg_max <- 1
+    
+    if (value==0 | is.na(value)){
+      scaled <- 0
+    } else if (value > 0){
+      scaled <- value/pos_max
+    } else {
+      scaled <- value/neg_max
+    }
+    
+    
+    # print("===========")
+    # print(col_name)
+    # print(value)
+    # print(pos_max)
+    # print(neg_max)
+    # print(scaled)
+    # 
+    display_val <- format_pct(value)
+    div(class = "details_col", style = list(background = p_m_colours(scaled)), display_val)
+  }
+}
