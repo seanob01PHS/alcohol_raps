e50eb9832d1f041e116bc7257b64b30b071cae58
seanob01PHS <sean.obrien@phs.scot>
2022-03-16 15:31:09 +0000

    initial commit to new stage of project. Some file stubs created

---
+++ make_qtr_report.R
@@ -0,0 +1,16 @@
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+#
+# MAIN (To create a new alcohol harms quarterly report, run this script only!)
+#
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+#{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+
+# This script should be in the following location for it to work
+# "/conf/LIST_analytics/Glasgow City/Drugs & Alcohol/Alcohol/Regular Report RAPs"
+
+# The start of the next quarter for which you want to run the report.
+qtr_start <- "2021-09-30"
+
+
