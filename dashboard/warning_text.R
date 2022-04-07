2799cf24a69fdc2e579fa08ab267d69f7064273f
seanob01PHS <sean.obrien@phs.scot>
2022-04-07 09:39:22 +0100

    css layout working and finalised

---
+++ dashboard/warning_text.R
@@ -0,0 +1,9 @@
+warning_text <- function(){
+  div(class = "warning_box",
+  paste("Management Information only, not for onward distribution.",
+        "This information has been released for management information purposes only.",
+        "The data have not been adjusted to protect against potential disclosure risks.",
+        "Please see the bottom of the \"General Notes\" tab under the \"Notes\" page."
+        )
+  )
+}
